2298b9e98ea7f5429dd8c2aae621ef7649ce61f9
Ezzahir Taha <taha1.ezzahir@gmail.com>
2023-05-06 21:47:24 +0200

    First Commit

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+
+const connectDB = async() => {
+    try {
+        const connect = await mongoose.connect('mongodb://127.0.0.1:27017/practice')
+        console.log(`MongoDB Connected:${connect.connection.host}`.cyan.underline.bold);
+    } catch (error) {
+        console.log("Can't connect to MongoDB !");
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 5000
+const colors = require('colors')
+const connectDB = require('./config/db')
+
+
+
+connectDB()
+
+app.get('/', (req, res) => {
+    res.send('Hello!')
+})
+
+app.listen(port, ()=> {
+    console.log(` App listening on http://localhost:${port}`);
+})
+
